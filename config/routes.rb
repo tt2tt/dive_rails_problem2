39f3ad00fbd5547e84d09860f9097008fcd74003
”taro <groveleffect@gmail.com>
2019-07-09 20:12:04 +0900

    ログアウト機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pictures#index'
+  
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :show, :create, :destroy]
   resources :pictures
