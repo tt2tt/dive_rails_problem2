a8977f4c3e2edeb7ff16d6cf0e8468eb4179447e
”taro <groveleffect@gmail.com>
2019-07-10 20:32:09 +0900

    共通レイアウトヘッダーの作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to user_path(user.id)
+      flash[:notice] = 'ログインしました'
     else
       flash[:danger] = 'ログインに失敗しました'
       render 'new'
