cf8e5009d9124bf13fc3df20854188d4a68f0a31
”taro <groveleffect@gmail.com>
2019-07-10 20:57:58 +0900

    Pictureモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190709010140) do
+ActiveRecord::Schema.define(version: 20190710114740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pictures", force: :cascade do |t|
+    t.text "content"
+    t.text "image"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pictures_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -24,4 +33,5 @@ ActiveRecord::Schema.define(version: 20190709010140) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "pictures", "users"
 end
