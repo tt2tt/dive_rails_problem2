9bc3b6f0f7a8f454782d4e0ad8e20fe7b3b15ca8
”taro <groveleffect@gmail.com>
2019-07-12 11:24:52 +0900

    一覧表示機能の追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,6 @@
 class PicturesController < ApplicationController
   def index
-
+    @pictures = Picture.all
   end
 
   def new
@@ -10,11 +10,20 @@ class PicturesController < ApplicationController
 
   def create
     @picture = current_user.pictures.build(picture_params)
+    if @picture.save
+      redirect_to pictures_path, notice: "投稿しました"
+    end
   end
+
+  def edit
+  end
+
+
 
   private
 
   def picture_params
     params.require(:picture).permit(:content, :image, :image_cache)
   end
+
 end
