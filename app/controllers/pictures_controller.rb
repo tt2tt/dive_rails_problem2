2739939f879764b83220ce586602e22315e1ecd6
”taro <groveleffect@gmail.com>
2019-07-12 10:18:38 +0900

    UserモデルとPictureモデルのアソシエーションの設定

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,6 +4,17 @@ class PicturesController < ApplicationController
   end
 
   def new
+    @picture = Picture.new
 
+  end
+
+  def create
+    @picture = current_user.pictures.build(picture_params)
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:content, :image)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 30}
   validates :email, presence: true, length: {maximum: 255},
                               format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+  has_many :pictures
+
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}
 
