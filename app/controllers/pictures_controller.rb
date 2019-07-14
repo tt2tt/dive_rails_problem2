3292aff3b3e52e644965d3ba860ec251ff4f93b7
”taro <groveleffect@gmail.com>
2019-07-14 13:39:47 +0900

    確認画面の作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -40,6 +40,6 @@ class PicturesController < ApplicationController
   end
 
   def picture_params
-    params.require(:picture).permit(:content, :image, :image_cache)
+    params.require(:picture).permit(:content, :image, :image_cache, [])
   end
 end
