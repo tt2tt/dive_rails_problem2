cb15c8c9ac1a8a4f60fafcfc6669e0d823916ba2
”taro <groveleffect@gmail.com>
2019-07-14 14:59:52 +0900

    Pictureモデルのバリテーションの追加

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -1,2 +1,9 @@
 module PicturesHelper
+  def choose_new_or_edit
+    if action_name == 'new' || action_name == 'confirm'
+      confirm_pictures_path
+    else action_name == 'edit'
+      picture_path
+    end
+  end
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,15 @@
 class Picture < ApplicationRecord
+  validates :content_or_image, presence: true
+  validates :content, length: { maximum: 140 }
+
+
   belongs_to :user
 
   mount_uploader :image, ImageUploader
+
+  private
+
+  def content_or_image
+    content.presence or image.presence
+  end
 end
