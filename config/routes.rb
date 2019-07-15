afda5a018eba864f3787d86d6e5c8f075a51ce1a
”taro <groveleffect@gmail.com>
2019-07-15 20:43:36 +0900

    デザインの調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pictures#index'
+  root to: 'users#new'
 
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :show, :create, :destroy]
